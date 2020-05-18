0f3c9125c541cc490b63c66caf29d6c4788c36f4
leysrem <remi.leyssene@gmail.com>
2020-05-18 19:02:52 +0200

    genre

--- ajouter-morceau.php
+++ ajouter-morceau.php
@@ -1,23 +1,7 @@
 <!DOCTYPE HTML>
 
-<?php
-	$dataBase = mysqli_connect ( "localhost" , "root" , "" ) ;
 
-	//--- Ouverture de la base de données
-	mysqli_select_db ( $dataBase, "djtools" ) ;
 
-	$requete = "select * from morceau;";
-
-	$resultat = mysqli_query($dataBase, $requete , MYSQLI_USE_RESULT) or die(mysql_error());
-
-	
-
-?>
-<!--
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Prologue by HTML5 UP</title>
@@ -71,105 +55,186 @@
 
 		</script>
 	</head>
-	<body class="is-preload">
+
+	<?php
+	  $dsn = 'mysql:dbname=djtools;host=localhost';
+	  $user = 'root';
+	  $password = '';
+  
+	  try {
+		  $db = new PDO($dsn, $user, $password);
+		   if ($db) {
+		
+  
+		   // Alerte a chaque fois qu'une requête échoue
+		   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        
+		   // Désactive la simulation des requêtes préparés, 
+		   // utilise l'interface native pour récupérer les données et leur type.
+		   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); 
+
+
+		   // Requete pour afficher la liste des albums
+			$RsqlAlbum = 'SELECT * FROM album';
+
+			$query = $db->prepare($RsqlAlbum);
+			$query->execute();
+			$albums = $query->fetchAll(PDO::FETCH_ASSOC);
+			
+
+			if (isset($_GET["option"]) == "ajouter") {
+
+				$nom_morceau = $_GET["titre-morceau"];
+				$nom_genre = $_GET["genre"];
+				$artistes = $_GET["artiste"];
+				$id_albums = $_GET["id_album"];
+				
+				$verifGenre = "SELECT * FROM genre WHERE nom_genre = '".$nom_genre."'";
+
+				$query = $db->prepare($verifGenre);
+				$query->execute();
+				$genres = $query->fetchAll(PDO::FETCH_ASSOC);
+
+			
+				// GESTION GENRE 
+				if(!count($genres) == 0) {	
+					// SI IL EXISTE
+					foreach($genres as $genre) 
+					{
+						$id_genre = $genre['id_genre'];
+					}
+					// AJOUTE LE GENRE AU NOUVEAU MORCEAU
+					$ajouterMorceau = "INSERT INTO morceau (titre_morceau, fk_id_genre) VALUES ('".$nom_morceau."','".$id_genre."')";
+					$query = $db->prepare($ajouterMorceau);
+					$query->execute();
+				} else {
+					// AJOUTER LE NOUVEAU GENRE
+					$ajouterGenre = "INSERT INTO genre (nom_genre) VALUES ('".$nom_genre."')";
+					$query = $db->prepare($ajouterGenre);
+					$query->execute();
+					// RECUP LE NOUVEAU GENRE
+					$getLastGenre = "SELECT MAX(id_genre) as id_genre FROM genre";
+					$query = $db->prepare($getLastGenre);
+					$query->execute();
+					$genres = $query->fetchAll(PDO::FETCH_ASSOC);
+
+					foreach($genres as $genre) 
+					{
+						$id_genre = $genre['id_genre'];
+					}
+					// AJOUTE LE GENRE AU NOUVEAU MORCEAU
+					$ajouterMorceau = "INSERT INTO morceau (titre_morceau, fk_id_genre) VALUES ('".$nom_morceau."','".$id_genre."')";
+					$query = $db->prepare($ajouterMorceau);
+					$query->execute();
+				}
+			}
+
+	echo "	
+	<body class='is-preload'>
 		<!-- Header -->
-			<div id="header">
-				<div class="top">
+			<div id='header'>
+				<div class='top'>
 
 					<!-- Logo -->
-					<div id="logo">
-						<h1 id="title">DJTools</h1>
+					<div id='logo'>
+						<h1 id='title'>DJTools</h1>
 					</div>
 
 					<!-- Nav -->
-					<nav id="nav">
+					<nav id='nav'>
 						<ul>
-							<li><a href="album.php"><span class="icon solid fa-th">Albums</span></a></li>
-							<li><a href="playlist.php"><span class="icon solid fa-th">Playlist</span></a></li>
-							<li><a href="ajouter-morceau.php"><span class="icon solid fa-plus">Ajouter un morceau</span></a></li>
-							<li><a href="ajouter-album.php"><span class="icon solid fa-plus">Ajouter un album</span></a></li>
-							<li><a href="ajouter-playlist.php"><span class="icon solid fa-plus">Ajouter une playlist</span></a></li>
+							<li><a href='album.php'><span class='icon solid fa-th'>Albums</span></a></li>
+							<li><a href='playlist.php'><span class='icon solid fa-th'>Playlist</span></a></li>
+							<li><a href='ajouter-morceau.php'><span class='icon solid fa-plus'>Ajouter un morceau</span></a></li>
+							<li><a href='ajouter-album.php'><span class='icon solid fa-plus'>Ajouter un album</span></a></li>
+							<li><a href='ajouter-playlist.php'><span class='icon solid fa-plus'>Ajouter une playlist</span></a></li>
 						</ul>
 					</nav>
 				</div>
 			</div>
 
 		<!-- Main -->
-			<div id="main">
+			<div id='main'>
 				<!-- Portfolio -->
-					<section id="portfolio" class="two">
-						<div class="container">
-							<div class="row">
-								<div class="col-12 col-12-mobile">
-									<article class="item" id='1'>
-										<form class="addform" action="" method="get">
+					<section id='portfolio' class='two'>
+						<div class='container'>
+							<div class='row'>
+								<div class='col-12 col-12-mobile'>
+									<article class='item' id='1'>
+										<form class='addform' action='' method='get'>
 											<h2>Ajouter un morceau</h2>
 											<fieldset>
-												<input class='input-upload' id='le-morceau' type="file" required accept="audio/mp3, audio/mp4, audio/wma, audio/ogg, audio/wav">
+												<input class='input-upload' id='le-morceau' type='file' required accept='audio/mp3, audio/mp4, audio/wma, audio/ogg, audio/wav'>
 											</fieldset>
-											<input placeholder="Nom du morceau" type="text" tabindex="1" name='titre-morceau' required autofocus> 
+											<input placeholder='Nom du morceau' type='text' tabindex='1' name='titre-morceau' required autofocus> 
 											<hr>
 											<fieldset>
-												<div class="wrapper-genres">
-													<input placeholder="Selectionner le genre" name="genre" list="Genres" class="genres" required/>
+												<div class='wrapper-genres'>
+													<input placeholder='Selectionner le genre' name='genre' list='Genres' class='genres' required/>
+													<datalist id='Genres' tabindex='2' required>
+													";
 
-													<datalist id="Genres" tabindex="2" required>
-														<option value="Exemple genre 1">
-														<option value="Exemple genre 2">
-														<option value="Exemple genre 3">
-														<option value="Exemple genre 4">
-														<option value="Exemple genre 5">
+													$genres = 'SELECT * FROM genre';
+
+													$query = $db->prepare($genres);
+													$query->execute();
+													$genres = $query->fetchAll(PDO::FETCH_ASSOC);
+
+													foreach ($genres as $row => $genre) 
+													{
+
+													echo "
+														<option value='".$genre['nom_genre']."'>
+														";
+													}
+												echo "
 													</datalist>
 												</div>
 											</fieldset>	
 											<hr>
 											<fieldset>
-												<button type="button" class='modifier-artistes-btn' id="ajouter-artiste" onclick="modifier_artistes(this.id)">Ajouter un artiste</button>
-												<button type="button" class='modifier-artistes-btn' id="supprimer-artiste" onclick="modifier_artistes(this.id)" disabled>Supprimer un artiste</button>
+												<button type='button' class='modifier-artistes-btn' id='ajouter-artiste' onclick='modifier_artistes(this.id)'>Ajouter un artiste</button>
+												<button type='button' class='modifier-artistes-btn' id='supprimer-artiste' onclick='modifier_artistes(this.id)' disabled>Supprimer un artiste</button>
 											</fieldset>
 											<fieldset>
-												<div class="wrapper-artistes">
-													<input placeholder="Selectionner l artiste" name="artiste[]" list="Artistes" id="Artiste-1" class="artistes" required/>
+												<div class='wrapper-artistes'>
+													<input placeholder='Selectionner l artiste' name='artiste[]' list='Artistes' id='Artiste-1' class='artistes' required/>
 
-													<datalist id="Artistes" tabindex="2" required>
-														<option value="Exemple artiste 1">
-														<option value="Exemple artiste 2">
-														<option value="Exemple artiste 3">
-														<option value="Exemple artiste 4">
-														<option value="Exemple artiste 5">
+													<datalist id='Artistes' tabindex='2' required>
+														<option value='Exemple artiste 1'>
+														<option value='Exemple artiste 2'>
+														<option value='Exemple artiste 3'>
+														<option value='Exemple artiste 4'>
+														<option value='Exemple artiste 5'>
 													</datalist>
 												</div>
 											</fieldset>
 											<hr>
 											<fieldset>
-												<input type="text" id="myInputSearchBar" placeholder="Rechercher un album ..." title="Type in a name" class='input-search-bar'>
+												<input type='text' id='myInputSearchBar' placeholder='Rechercher un album ...' title='Type in a name' class='input-search-bar'>
 											</fieldset>
 
 											<div class='item-container'>
 											<table>
-												<tbody id='album-item-list'>
-													<tr class="item-album" id='item-album-1'>
-														<td>Deux freres</td>
-														<td>PNL</td>
-														<td><input type="checkbox" name='item-album' value='Deux freres' id='item-album-1-checkbox'/></td>
+												<tbody id='album-item-list'>";
+												
+												foreach ($albums as $row => $album) 
+												{
+												echo "
+													<tr class='item-album' id='item-album-".$album['id_album']."'>
+														<td>".$album['nom_album']."</td>
+														<td><input type='checkbox' name='id_album[]' value='".$album['id_album']."' id='item-album-".$album['id_album']."-checkbox'/></td>
 													</tr>
-													<tr class="item-album" id='item-album-2'>
-														<td>Polak</td>
-														<td>PLK</td>
-														<td><input type="checkbox" name='item-album' value='Polak' id='item-album-2-checkbox'/></td>
-													</tr>
-													<tr class="item-album" id='item-album-3'>
-														<td>Commando</td>
-														<td>Niska</td>
-														<td><input type="checkbox" name='item-album' value='Commando' id='item-album-3-checkbox'/></td>
-													</tr>
+														";
+												}
+											echo "	
 												</tbody>
 											</table>
 											</div>	
 						
 												</table>
 												<fieldset>
-													<button type="submit" id="ajouter-morceau">Ajouter</button>
+													<button type='submit' name='option' value='ajouter' id='ajouter-morceau'>Ajouter</button>
 											  </fieldset>
 										</form>
 									</article>
@@ -180,12 +245,21 @@
 				</div>
 
 		<!-- Scripts -->
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src='assets/js/jquery.scrolly.min.js'></script>
+			<script src='assets/js/jquery.scrollex.min.js'></script>
+			<script src='assets/js/browser.min.js'></script>
+			<script src='assets/js/breakpoints.min.js'></script>
+			<script src='assets/js/util.js'></script>
+			<script src='assets/js/main.js'></script>
 
 	</body>
+
+		";
+		  }
+	  } catch (PDOException $e) {
+		  $e->getMessage();
+	  }
+
+
+?>
 </html>
